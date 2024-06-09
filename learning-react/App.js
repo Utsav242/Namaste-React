d186379e15fa65a09b1ad51ff57b361f11f2ab75
Utsav34 <utsavgoel786@gmail.com>
2024-06-09 17:16:51 +0530

    learn jsx and functional component

--- learning-react/App.js
+++ learning-react/App.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React from "react";
 import ReactDOM from 'react-dom/client';
 
-const heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  "Hello world from React!"
-);
-const root = ReactDOM.createRoot(document.getElementById("root"));
+// React Element
 
-root.render(heading);
+const heading =(
+    <h1>Namaste React</h1>
+);
+
+// Function comp
+
+const FunctionComp=()=>{
+    return(
+        <>
+        <h1 className="heading"> Function component</h1>
+        </>
+    )
+}
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(<FunctionComp />)
