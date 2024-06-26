829452e8294ed8b9e341cb807add01d84e1bd8cd
Utsav34 <utsavgoel786@gmail.com>
2024-06-26 20:12:49 +0530

    resolved wsigy api issue

--- learning-react/src/components/ResturantCard.js
+++ learning-react/src/components/ResturantCard.js
@@ -1,24 +1,24 @@
+import { CDN_URL } from "../utils/constant";
+
 const ResturantCard = ({ restData }) => {
-    // Destructring props
-  
-    const { title, price, rating, image } = restData;
-  const IMG_URL = 'https://fakestoreapi.com/'
-    return (
-      <div className="restroCard">
-        <div className="foodImg">
-          <img
-            src={image}
-            alt="imgLogo"
-          />
-        </div>
-        <div className="foodDetail">
-          <h4>{title}</h4>
-          <h5>{price}</h5>
-          <p>{rating.rate}</p>
-        </div>
+  // Destructring props
+
+  const { cloudinaryImageId, costForTwo, name, cuisines, avgRating } =
+    restData.info;
+
+  return (
+    <div className="restroCard">
+      <div className="foodImg">
+        <img src={CDN_URL + cloudinaryImageId} alt="foodImg" />
       </div>
-    );
-  };
+      <div className="foodDetail">
+        <h4>{name}</h4>
+        <h5>{cuisines.join(", ")}</h5>
+        <p>{costForTwo}</p>
+        <span>{avgRating} Stars</span>
+      </div>
+    </div>
+  );
+};
 
-
-  export default ResturantCard;
+export default ResturantCard;
